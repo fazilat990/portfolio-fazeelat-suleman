1f827260dfd01680de87f0b326f98a4b812e1c13
Fazilat Suleman <fazilat.990@gmail.com>
2023-02-12 19:24:00 +0500

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import logo from '../logo.svg';
 function Header() {
   return (
     <div>
-      <header>
-        <div className="container">
+      <header className='fixed top-0 left-0 right-0 z-50'>
+        <div className="container mx-auto px-4 py-6">
             <img src={logo} alt='Fazeelat Suleman' />
         </div>
       </header>
